824f3352002800ecffddd07b2af559b2273e8412
Chris Brulak <chris.brulak@ama.ab.ca>
2012-07-30 10:36:31 -0600

    fixing meta

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,6 @@ maintainer       "Alberta Motor Association"
 maintainer_email "webmaster@ama.ab.ca"
 license          "Apache 2.0"
 description      "Mounts a S3 bucket as file system. Forked from https://github.com/twilson63/s3fs-recipe"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 
