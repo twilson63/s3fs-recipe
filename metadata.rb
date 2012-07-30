3e64dfddc4a7eb8c7320e4f2c7f0f04f4a7cd38e
Chris Brulak <chris.brulak@ama.ab.ca>
2012-07-30 10:35:37 -0600

    adding meta file

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "Alberta Motor Association"
+maintainer_email "webmaster@ama.ab.ca"
+license          "Apache 2.0"
+description      "Mounts a S3 bucket as file system. Forked from https://github.com/twilson63/s3fs-recipe"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+
+%w{ubuntu}.each do |os|
+  supports os
+end
+
+recipe           "s3fs", "Mounts a S3 bucket as file system. Forked from https://github.com/twilson63/s3fs-recipe"
