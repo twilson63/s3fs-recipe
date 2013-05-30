5c70014b0a66aefc1dc57eb92bb9805b31359a01
wohali <joant@ieee.org>
2013-05-30 17:35:14 -0400

    Modernize recipe and foodcritic cleanup

--- s3fs/recipes/default.rb
+++ s3fs/recipes/default.rb
@@ -1,20 +1,20 @@
-node[:s3fs][:packages].each do |pkg|
+node['s3fs']['packages'].each do |pkg|
   package pkg
 end
 
-if not node[:s3fs][:packages].include?("fuse")
+if not node['s3fs']['packages'].include?("fuse")
   # install fuse
-  remote_file "/tmp/fuse-#{ node[:fuse][:version] }.tar.gz" do
-    source "http://downloads.sourceforge.net/project/fuse/fuse-2.X/#{ node[:fuse][:version] }/fuse-#{ node[:fuse][:version] }.tar.gz"
+  remote_file "#{Chef::Config[:file_cache_path]}/fuse-#{ node['fuse']['version'] }.tar.gz" do
+    source "http://downloads.sourceforge.net/project/fuse/fuse-2.X/#{ node['fuse']['version'] }/fuse-#{ node['fuse']['version'] }.tar.gz"
     mode 0644
     action :create_if_missing
   end
 
   bash "install fuse" do
-    cwd "/tmp"
+    cwd Chef::Config[:file_cache_path]
     code <<-EOH
-    tar zxvf fuse-#{ node[:fuse][:version] }.tar.gz
-    cd fuse-#{ node[:fuse][:version] }
+    tar zxvf fuse-#{ node['fuse']['version'] }.tar.gz
+    cd fuse-#{ node['fuse']['version'] }
     ./configure --prefix=/usr
     make
     make install
@@ -26,7 +26,7 @@ if not node[:s3fs][:packages].include?("fuse")
 
 end
 
-if %w{centos redhat amazon}.include?(node["platform"])
+if %w{centos redhat amazon}.include?(node['platform'])
   template "/etc/ld.so.conf.d/s3fs.conf" do
     source "s3fs.conf.erb"
     owner "root"
@@ -40,18 +40,18 @@ if %w{centos redhat amazon}.include?(node["platform"])
   end
 end
 
-remote_file "/tmp/s3fs-#{ node[:s3fs][:version] }.tar.gz" do
-  source "http://s3fs.googlecode.com/files/s3fs-#{ node[:s3fs][:version] }.tar.gz"
+remote_file "#{Chef::Config[:file_cache_path]}/s3fs-#{ node['s3fs']['version'] }.tar.gz" do
+  source "http://s3fs.googlecode.com/files/s3fs-#{ node['s3fs']['version'] }.tar.gz"
   mode 0644
   action :create_if_missing
 end
 
 bash "install s3fs" do
-  cwd "/tmp"
+  cwd Chef::Config[:file_cache_path]
   code <<-EOH
   export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/lib64/pkgconfig
-  tar zxvf s3fs-#{ node[:s3fs][:version] }.tar.gz
-  cd s3fs-#{ node[:s3fs][:version] }
+  tar zxvf s3fs-#{ node['s3fs']['version'] }.tar.gz
+  cd s3fs-#{ node['s3fs']['version'] }
   ./configure --prefix=/usr
   make
   make install
@@ -60,7 +60,7 @@ bash "install s3fs" do
   not_if { File.exists?("/usr/bin/s3fs") }
 end
 
-s3_bag = data_bag_item(node[:s3fs][:data_bag][:name], node[:s3fs][:data_bag][:item])
+s3_bag = data_bag_item(node['s3fs']['data_bag']['name'], node['s3fs']['data_bag']['item'])
 
 template "/etc/passwd-s3fs" do
   source "passwd-s3fs.erb"
@@ -70,7 +70,7 @@ template "/etc/passwd-s3fs" do
   variables(:s3_bag => s3_bag)
 end
 
-s3_bag["buckets"].each do |bucket|
+s3_bag['buckets'].each do |bucket|
   directory "/mnt/#{ bucket}" do
     owner     "root"
     group     "root"
@@ -84,7 +84,7 @@ s3_bag["buckets"].each do |bucket|
   mount "/mnt/#{ bucket}" do
     device "s3fs##{ bucket}"
     fstype "fuse"
-    options "#{node[:s3fs][:options]}"
+    options node['s3fs']['options']
     dump 0
     pass 0
     action [:mount, :enable]
