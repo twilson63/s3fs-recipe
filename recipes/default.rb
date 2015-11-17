95b757ab1d06979722d39094c6948dcc8791d069
Pascal Schneider <darkgigabyte@googlemail.com>
2015-11-17 14:35:12 +0100

    fixed file, directory and version name

--- recipes/default.rb
+++ recipes/default.rb
@@ -60,8 +60,8 @@ if %w{centos redhat amazon}.include?(node['platform'])
   end
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/s3fs-#{ node['s3fs']['version'] }.tar.gz" do
-  source "https://github.com/s3fs-fuse/s3fs-fuse/archive/#{ node['s3fs']['version'] }.tar.gz"
+remote_file "#{Chef::Config[:file_cache_path]}/s3fs-fuse-#{ node['s3fs']['version'] }.tar.gz" do
+  source "https://github.com/s3fs-fuse/s3fs-fuse/archive/v#{ node['s3fs']['version'] }.tar.gz"
   mode 0644
   action :create_if_missing
 end
@@ -70,8 +70,8 @@ bash "install s3fs" do
   cwd Chef::Config[:file_cache_path]
   code "
     export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/lib64/pkgconfig
-    tar zxvf s3fs-#{ node['s3fs']['version'] }.tar.gz
-    cd s3fs-#{ node['s3fs']['version'] }
+    tar zxvf s3fs-fuse-#{ node['s3fs']['version'] }.tar.gz
+    cd s3fs-fuse-#{ node['s3fs']['version'] }
     ./autogen.sh
     ./configure --prefix=/usr
     make
