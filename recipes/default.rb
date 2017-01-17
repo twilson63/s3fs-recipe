b674df1c38d050b930b0f50f449cb5cd11972e3f
Yorgos Saslis <g.saslis@gmail.com>
2017-01-17 14:56:12 +0200

    Fixed indentation

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,17 +28,17 @@ end
 
 if node['s3fs']['build_from_source'] == true
 
-if not node['s3fs']['packages'].include?("fuse")
-  # install fuse
-  remote_file "#{Chef::Config[:file_cache_path]}/fuse-#{ node['fuse']['version'] }.tar.gz" do
-    source uri_join(node['s3fs']['fuse']['uri'],"#{get_release_path(node['fuse']['version'])}/fuse-#{ node['fuse']['version'] }.tar.gz").to_s
-    mode 0644
-    action :create_if_missing
-  end
+  if not node['s3fs']['packages'].include?("fuse")
+    # install fuse
+    remote_file "#{Chef::Config[:file_cache_path]}/fuse-#{ node['fuse']['version'] }.tar.gz" do
+      source uri_join(node['s3fs']['fuse']['uri'], "#{get_release_path(node['fuse']['version'])}/fuse-#{ node['fuse']['version'] }.tar.gz").to_s
+      mode 0644
+      action :create_if_missing
+    end
 
-  bash "install fuse" do
-    cwd Chef::Config[:file_cache_path]
-    code "
+    bash "install fuse" do
+      cwd Chef::Config[:file_cache_path]
+      code "
       tar zxvf fuse-#{ node['fuse']['version'] }.tar.gz
       cd fuse-#{ node['fuse']['version'] }
       ./configure --prefix=/usr
@@ -46,34 +46,34 @@ if not node['s3fs']['packages'].include?("fuse")
       make install
     "
 
-    not_if { File.exists?("/usr/bin/fusermount") }
+      not_if { File.exists?("/usr/bin/fusermount") }
+    end
+
   end
 
-end
+  if %w{centos redhat amazon}.include?(node['platform'])
+    template "/etc/ld.so.conf.d/s3fs.conf" do
+      source "s3fs.conf.erb"
+      owner "root"
+      group "root"
+      mode 0644
+    end
 
-if %w{centos redhat amazon}.include?(node['platform'])
-  template "/etc/ld.so.conf.d/s3fs.conf" do
-    source "s3fs.conf.erb"
-    owner "root"
-    group "root"
+    bash "ldconfig" do
+      code "ldconfig"
+      only_if { `ldconfig -v | grep fuse | wc -l` == 0 }
+    end
+  end
+
+  remote_file "#{Chef::Config[:file_cache_path]}/s3fs-fuse-#{ node['s3fs']['version'] }.tar.gz" do
+    source uri_join(node['s3fs']['uri'],"v#{ node['s3fs']['version'] }.tar.gz").to_s
     mode 0644
+    action :create_if_missing
   end
 
-  bash "ldconfig" do
-    code "ldconfig"
-    only_if { `ldconfig -v | grep fuse | wc -l` == 0 }
-  end
-end
-
-remote_file "#{Chef::Config[:file_cache_path]}/s3fs-fuse-#{ node['s3fs']['version'] }.tar.gz" do
-  source uri_join(node['s3fs']['uri'],"v#{ node['s3fs']['version'] }.tar.gz").to_s
-  mode 0644
-  action :create_if_missing
-end
-
-bash "install s3fs" do
-  cwd Chef::Config[:file_cache_path]
-  code "
+  bash "install s3fs" do
+    cwd Chef::Config[:file_cache_path]
+    code "
     export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/lib64/pkgconfig
     tar zxvf s3fs-fuse-#{ node['s3fs']['version'] }.tar.gz
     cd s3fs-fuse-#{ node['s3fs']['version'] }
@@ -83,8 +83,8 @@ bash "install s3fs" do
     make install
   "
 
-  not_if { File.exists?("/usr/bin/s3fs") }
-end
+    not_if { File.exists?("/usr/bin/s3fs") }
+  end
 
 end
 
